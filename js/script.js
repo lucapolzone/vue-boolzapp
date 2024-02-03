1cfcb404c36c67396311376f073bc0cef1db556e
lucapolzone <lucapolzone@gmail.com>
2024-02-03 19:21:18 +0100

    feat: add milestone 2

--- js/script.js
+++ js/script.js
@@ -4,8 +4,33 @@ const app = createApp({
   data() {
     return {
       contacts,
+      activeIndex: 0,
     };
   },
+
+  computed: {
+    activeContact() {
+      return this.contacts[this.activeIndex];
+    },
+  },
+
+
+  methods: {
+    getLastAccessFromMessages(messages) {
+      const sentMessages = messages.filter((message) => message.status == 'sent');
+      const lastMessage = sentMessages[sentMessages.length - 1];
+      return lastMessage ? lastMessage.date : ''; 
+    },
+
+    getLastMessageFromMessages(messages) {
+      const lastMessage = messages.at(-1);
+      return lastMessage ? lastMessage.message : ''; 
+    },
+
+    setActiveIndex(newIndex) {
+      this.activeIndex = newIndex;
+    },
+  }
 });
 
-app.mount("#big-wrapper");
+app.mount('#big-wrapper');
