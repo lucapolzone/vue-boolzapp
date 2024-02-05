c422090bb9a967c66ebb8f025d9ea22616398fa1
lucapolzone <lucapolzone@gmail.com>
2024-02-05 21:23:14 +0100

    feat: add milestone 4

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const app = createApp({
     return {
       contacts,
       activeIndex: 0,
+      visible: true, //per il contatto in contact list
+      contactsTerm: '',
       newMessage: {
         date: '',
         message: '',
@@ -15,34 +17,43 @@ const app = createApp({
 
   computed: {
     activeContact() {
+      //lego il cambio dell'indice al cambio di contatto
       return this.contacts[this.activeIndex];
     },
   },
 
 
   methods: {
+    //ultimo accesso
     getLastAccessFromMessages(messages) {
       const sentMessages = messages.filter((message) => message.status == 'sent');
       const lastMessage = sentMessages[sentMessages.length - 1];
       return lastMessage ? lastMessage.date : ''; 
     },
 
+    //ultimo messaggio ricevuto
     getLastMessageFromMessages(messages) {
       const lastMessage = messages.at(-1);
       return lastMessage ? lastMessage.message : ''; 
     },
 
+    //funzione per gestire il cambio dell'indice
     setActiveIndex(newIndex) {
       this.activeIndex = newIndex;
     },
     
+    //funzione che permette all'utente di scrivere un messaggio
     sendMessage() {
+      //interrompe la funzione se il testo è vuoto
+      if(!this.newMessage.message) return;
+
       const newMessage = {...this.newMessage};
       newMessage.date = this.getCurrentTime();
       this.activeContact.messages.push(newMessage);
       setTimeout(this.sendAutomatedResponse, 1000);
     },
 
+    //risposta automatica
     sendAutomatedResponse() {
       const newMessage = {
         message: 'Ok',
@@ -53,8 +64,21 @@ const app = createApp({
     },
 
     getCurrentTime() {
+      //creo un nuovo oggetto che gestisce ore e minuti
       const now = new Date();
       return `${now.getHours()}:${now.getMinutes()}`;
+    },
+
+    contactsFilter() {
+      //sovrascrivo contacts con contacts.map
+      this.contacts = this.contacts.map((contact) => {
+       if (contact.name.toLowerCase().includes(this.contactsTerm.toLowerCase())) {
+         contact.visible = true;
+        } else {
+         contact.visible = false;
+        }
+        return contact;
+      })
     }
   }
 });
